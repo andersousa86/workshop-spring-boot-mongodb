b3c23b1e526823acaf8ff3524aacb97033be4c66
Anderson de Sousa <andersousa86@gmail.com>
2023-03-11 17:26:52 -0300

    Obtendo um post por id

---
+++ src/main/java/com/anderson/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.anderson.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.anderson.workshopmongo.domain.Post;
+import com.anderson.workshopmongo.repository.PostRepository;
+import com.anderson.workshopmongo.services.exception.ObjectNotException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotException("Objeto nao encontrado"));
+	}
+	
+}
